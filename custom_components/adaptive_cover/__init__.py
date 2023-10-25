a2597e5341bcb8bf3512849e64763391d48737b7
basbruss <68892092+basbruss@users.noreply.github.com>
2023-10-25 12:54:30 +0200

    fix options

--- custom_components/adaptive_cover/__init__.py
+++ custom_components/adaptive_cover/__init__.py
@@ -27,9 +27,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, (Platform.SENSOR,))
 
     await async_initialize_integration(hass=hass, config_entry=entry)
-    
+
     entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
-    
+
     return True
 
 
@@ -44,6 +44,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(
         entry, (Platform.SENSOR,)
     ):
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+        return unload_ok
