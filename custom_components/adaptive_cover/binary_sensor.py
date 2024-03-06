efc85d8be474c0f5d5d2da95ccdd5001bce25556
basbruss <68892092+basbruss@users.noreply.github.com>
2024-03-06 10:20:06 +0100

    add binary_sensor platform

---
+++ custom_components/adaptive_cover/binary_sensor.py
@@ -0,0 +1,83 @@
+"""Demo platform that has two fake binary sensors."""
+
+from __future__ import annotations
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import CONF_SENSOR_TYPE, DOMAIN
+from .coordinator import AdaptiveDataUpdateCoordinator
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the demo binary sensor platform."""
+    coordinator: AdaptiveDataUpdateCoordinator = hass.data[DOMAIN][
+        config_entry.entry_id
+    ]
+
+    binary_sensor = AdaptiveCoverBinarySensor(
+        config_entry,
+        config_entry.entry_id,
+        "Sun Infront",
+        False,
+        BinarySensorDeviceClass.MOTION,
+        coordinator,
+    )
+    async_add_entities([binary_sensor])
+
+
+class AdaptiveCoverBinarySensor(
+    CoordinatorEntity[AdaptiveDataUpdateCoordinator], BinarySensorEntity
+):
+    """representation of a Demo binary sensor."""
+
+    _attr_has_entity_name = True
+    _attr_should_poll = False
+
+    def __init__(
+        self,
+        config_entry,
+        unique_id: str,
+        binary_name: str,
+        state: bool,
+        device_class: BinarySensorDeviceClass,
+        coordinator: AdaptiveDataUpdateCoordinator,
+    ) -> None:
+        """Initialize the demo sensor."""
+        super().__init__(coordinator=coordinator)
+        self.type = {
+            "cover_blind": "Vertical",
+            "cover_awning": "Horizontal",
+            "cover_tilt": "Tilt",
+        }
+        self._name = config_entry.data["name"]
+        self._device_name = self.type[config_entry.data[CONF_SENSOR_TYPE]]
+        self._binary_name = binary_name
+        self._attr_unique_id = unique_id
+        self._state = state
+        self._attr_device_class = device_class
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, unique_id)},
+            name=self._device_name,
+        )
+
+    @property
+    def name(self):
+        """Name of the entity."""
+        return f"{self._binary_name} {self._name}"
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if the binary sensor is on."""
+        return self._state
