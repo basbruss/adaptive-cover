293f086bef9f73dd978c09b0f3f6570af8c8823a
basbruss <68892092+basbruss@users.noreply.github.com>
2024-03-06 12:15:52 +0100

    add control method sensor

--- custom_components/adaptive_cover/coordinator.py
+++ custom_components/adaptive_cover/coordinator.py
@@ -146,6 +146,14 @@ class AdaptiveDataUpdateCoordinator(DataUpdateCoordinator[AdaptiveCoverData]):
             default_state = 100 - default_state
             climate_state = 100 - climate_state
 
+        climate_data = ClimateCoverState(cover_data, climate).climate_data
+
+        control_method = "intermediate"
+        if climate_data.is_summer:
+            control_method = "summer"
+        if climate_data.is_winter:
+            control_method = "winter"
+
         return AdaptiveCoverData(
             climate_mode_toggle=self.switch_mode,
             states={
@@ -153,6 +161,7 @@ class AdaptiveDataUpdateCoordinator(DataUpdateCoordinator[AdaptiveCoverData]):
                 "climate": climate_state,
                 "start": NormalCoverState(cover_data).cover.solar_times()[0],
                 "end": NormalCoverState(cover_data).cover.solar_times()[1],
+                "control": control_method,
             },
             attributes={
                 "default": self.config_entry.options.get(CONF_DEFAULT_HEIGHT),
