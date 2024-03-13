3b807cd0ed38588fb1391e6e1b9b77942e76ddc4
basbruss <68892092+basbruss@users.noreply.github.com>
2024-03-13 20:37:24 +0100

    Fix Config entry was never loaded

--- custom_components/adaptive_cover/__init__.py
+++ custom_components/adaptive_cover/__init__.py
@@ -61,10 +61,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    if _climate_mode:
-        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS_SW)
-    else:
-        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    # if _climate_mode:
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS_SW)
+    # else:
+    #     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     return True
@@ -72,8 +72,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS_SW)
-    if unload_ok:
+    # platforms = PLATFORMS
+    # _climate_mode = entry.options.get(CONF_CLIMATE_MODE)
+    # if _climate_mode:
+    #     platforms = PLATFORMS_SW
+    if unload_ok := await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS_SW
+    ):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
